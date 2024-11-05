a6b7742081d766be9eeb5f785894cd93ae426a83
anihar2003 <itsnihara@gmail.com>
2024-11-05 13:42:48 +0530

    push

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-console.log('Server is running');
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/views')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
